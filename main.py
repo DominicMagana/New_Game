5da68b764a5fa64883e78e0f0e4ad2c14d1ad3da
DominicMagana <127146887+DominicMagana@users.noreply.github.com>
2023-03-31 10:24:26 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# File created by: Dominic
+# File created by: Chris Cozort
 # Agenda:
 # gIT GITHUB    
 # Build file and folder structures
@@ -6,7 +6,7 @@
 # testing github changes
 # I changed something - I changed something else tooooo!
 
-# This file was created by: DOminic
+# This file was created by: Chris Cozort
 # Sources: http://kidscancode.org/blog/2016/08/pygame_1-1_getting-started/
 # Sources: 
 
@@ -41,9 +41,17 @@ class Game:
         self.platforms = pg.sprite.Group()
         self.enemies = pg.sprite.Group()
         self.player = Player(self)
-        self.plat1 = Platform(WIDTH, 50, 0, HEIGHT-50, (150,150,150))
+        self.plat1 = Platform(WIDTH, 50, 0, HEIGHT-50, (150,150,150), "normal")
+        # self.plat1 = Platform(WIDTH, 50, 0, HEIGHT-50, (150,150,150), "normal")
         self.all_sprites.add(self.plat1)
+
+        self.platforms.add(self.plat1)
+        
         self.all_sprites.add(self.player)
+        for plat in PLATFORM_LIST:
+            p = Platform(*plat)
+            self.all_sprites.add(p)
+            self.platforms.add(p)
         for i in range(0,10):
             m = Mob(20,20,(0,255,0))
             self.all_sprites.add(m)
@@ -68,6 +76,18 @@ class Game:
                     self.player.jump()
     def update(self):
         self.all_sprites.update()
+        if self.player.vel.y > 0:
+            hits = pg.sprite.spritecollide(self.player, self.platforms, False)
+            if hits:
+                if hits[0].variant == "disappearing":
+                    hits[0].kill()
+                elif hits[0].variant == "bouncey":
+                    self.player.pos.y = hits[0].rect.top
+                    self.player.vel.y = -PLAYER_JUMP
+                else:
+                    self.player.pos.y = hits[0].rect.top
+                    self.player.vel.y = 0
+
     def draw(self):
         self.screen.fill(BLUE)
         self.all_sprites.draw(self.screen)
