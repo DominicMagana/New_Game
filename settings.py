a3e7c0a8206897906420bfa7fb6b8127da4a5789
DominicMagana <127146887+DominicMagana@users.noreply.github.com>
2023-04-06 23:40:24 -0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,9 +1,9 @@
 WIDTH = 800
 HEIGHT = 600
-PLAYER_ACC = 2
+PLAYER_ACC = .4
 PLAYER_FRICTION = -0.3
-PLAYER_JUMP = 20
-PLAYER_GRAV = 0.8
+PLAYER_JUMP = 10
+PLAYER_GRAV = 1
 MOB_ACC = 2
 MOB_FRICTION = -0.3
 BLACK = (0,0,0)
@@ -17,7 +17,7 @@ PAUSED = False
 
 # Starting platforms
 PLATFORM_LIST = [(0, HEIGHT - 40, WIDTH, 40, (200,200,200), "normal"),
-                 (WIDTH / 2 - 50, HEIGHT * 3 / 4, 100, 20, (200,200,200), "bouncey"),
-                 (125, HEIGHT - 350, 100, 5, (200,200,200), "disappearing "),
-                 (350, 200, 100, 20, (200,200,200), "normal"),
-                 (175, 100, 50, 20, (200,200,200), "normal")]
+                 (WIDTH / 2 - 50, HEIGHT * 3 / 5, 100, 20, (200,200,200), "bouncey"),
+                 (500, HEIGHT - 350, 100, 5, (200,200,200), "disappearing "),
+                 (190, 450, 100, 20, (200,200,200), "normal"),
+                 (105, 500, 50, 20, (100,100,100), "normal")]
